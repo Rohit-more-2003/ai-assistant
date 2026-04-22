938abf938e6ea7e915a6a6de31ec16fb02df70cf
Rohit_More <morer4851@gmail.com>
2026-04-22 12:53:28 +0530

    updated app.jsx for better ui structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,45 @@ import "./App.css"
 export default function App() {
 
   // ------------------ STATE ------------------
-  const [messages, setMessages] = useState(() => {
-    const saved = localStorage.getItem("chat");
-    return saved ? JSON.parse(saved) : [];
-  });
-
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const chatEndRef = useRef(null);
+
+    // ----------------- EFFECTS -----------------
+
+  // --- Load from localstorage ---
+  useEffect(() => {
+    const saved = localStorage.getItem("chat");
+
+    if(saved){
+      setMessages(JSON.parse(saved));
+    }
+  }, []);
+
+  // --- Save to localstorage ---
+  useEffect(() => {
+    localStorage.setItem("chat", JSON.stringify(messages));
+  }, [messages]);
+  
+  // --- Auto Scroll ---
+  useEffect(() => {
+    chatEndRef.current?.scrollIntoView({behavior: "smooth"});
+  }, [messages]);
+
 
   // ------------------ FUNCTIONS ------------------
   const handleSend = async () => {
     if (!input.trim() || loading) return;
 
-    const userMessage = { role: "user", content: input };
-    setMessages((prev) => [...prev, userMessage]);
-
+    const updatedMessage = [
+      ...messages,
+      { role: "user", content: input }
+    ];
+    
+    setMessages(updatedMessage);
+    setInput("")
     setLoading(true);
 
     try{  
@@ -33,85 +55,79 @@ export default function App() {
         },
         body: JSON.stringify({ 
           message: input.trim(),
-          history: messages
+          history: updatedMessage  // not messages, as setMessages is to scheduled to update messages this loop, not already updated
         })
       });
 
       const data = await res.json();
-
-      const aiMessage = { role: "ai", content: data.response };
-      setMessages((prev) => [...prev, aiMessage]);
-    }
-    catch(err){
-      setMessages((prev) => [
+      
+      setMessages(prev => [
         ...prev,
-        {role: "ai", content: "Error: Unable to connect to server."}
+        {
+          role: "ai",
+          content: data.response,
+          tool: data.tool || null
+        }
       ]);
+    } catch(err){
+      console.error("Error: ", err);
+    } finally{
+      setLoading(false);
     }
-
-    setInput("");
-    setLoading(false);
   };
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter" && !loading){
-      handleSend();
-    }
-  }
 
   const handleClear = () => {
     setMessages([]);
     localStorage.removeItem("chat");
   }
 
-  // ----------------- Effects -----------------
-  useEffect(() => {
-    localStorage.setItem("chat", JSON.stringify(messages));
-  }, [messages]);
-  
-  useEffect(() => {
-    chatEndRef.current?.scrollIntoView({behaviour: "smooth"});
-  }, [messages]);
 
   // ------------------ UI ------------------
   return (
     <div className="container">
-      <h1 className="title">AI Chat</h1>
+
+      <div className="header">
+        <h2>AI Assistant</h2>
+        <button onClick={handleClear} className="clear-btn">Clear Chat</button>
+      </div>
 
       <div className="chat-box">
-        {messages.map((msg, index) => (
-          <div 
-            key={index}
-            className={msg.role === "user" ? "userMessage" : "aiMessage"}
-          >
-            {msg.content}
-          </div>  
+        {messages.map((msg, i) =>(
+          <div key={i} className={`message ${msg.role}`}>
+            
+            <div className="bubble">
+              {msg.content}
+            </div>
+
+            {msg.tool && (
+              <div className="tool-tag">
+                used: {msg.tool}
+              </div>
+            )}
+
+          </div>
         ))}
+
+        {loading &&(
+          <div className="message assistant">
+            <div className="bubble">Typing...</div>
+          </div>
+        )}
+
         <div ref={chatEndRef}></div>
       </div>
 
       <div className="input-row">
         <input
-          className="input"
+          type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          onKeyDown={handleKeyDown}
-          placeholder="Type message..."
-          disabled={loading}
+          placeholder="Type a message..."
+          onKeyDown={(e) => e.key === "Enter" && handleSend()}
         />
-
-        <button 
-          className="button" 
-          onClick={handleSend}
-          disabled={loading}
-        >
-          {loading ? "Loading....." : "Enter"}
-        </button>
+        <button onClick={handleSend}>Send</button>
       </div>
-
-      <button className="button" onClick={handleClear}>
-        Clear Chat
-      </button>
+      
     </div>
   );
 };
